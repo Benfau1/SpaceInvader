c26092e75c7213188b6a9e72bbdf0d7a726d0a89
Crivoi Dmitrii <dmitriicrivoi@gmail.com>
2023-05-10 16:57:36 +0200

    shuttle is ready

--- space_invader.ino
+++ space_invader.ino
@@ -1,9 +1,37 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Gamebuino-Meta.h>
+#include "spriteShuttle.h"
 
+
+/* Piskel data for "New Piskel" */
+
+
+Image PlayerIdling(PlayerIdlingData);
+//Player's coords and speed
+int move_x= 32;
+int move_y = 56;
+int speedX = 1;
+
+const int rightStop = 8;
+const int leftStop = 5;
+void setup()
+{
+  gb.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  while (!gb.update())
+    ;
+  gb.display.clear();
+  
+  
+  if (gb.buttons.repeat(BUTTON_RIGHT, 0) && move_x < gb.display.width() - rightStop )
+  {
+    move_x++;
+  }
+  if (gb.buttons.repeat(BUTTON_LEFT, 0) && move_x > 0 )
+  {
+    move_x--;
+  }
+  gb.display.drawImage(move_x,move_y, PlayerIdling);
 }
